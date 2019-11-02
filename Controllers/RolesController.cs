00287fc98fe359f7ba09794c85a904944def70e1
Moxi <42675676+Moxank6277@users.noreply.github.com>
2019-11-02 20:48:09 +0530

    6

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 
 namespace Live_Quiz.Controllers
 {
-
+    [Authorize]
     public class RolesController : Controller
     {
         private static ApplicationDbContext context = new ApplicationDbContext();
@@ -77,6 +77,66 @@ namespace Live_Quiz.Controllers
             context.SaveChanges();
             //roleManager.Update(new IdentityRole() { Name=roles.Name,Id=roles.Id });
             return View(roles);
+        }
+        [HttpGet]
+        public ActionResult AssignRole(string role)
+        {
+            var allroles = roleManager.Roles.Select(x => x);
+            ViewBag.roles = allroles.ToList();
+            if (role==null)
+            {
+                ViewBag.isRoleSelected = false;
+                return View();
+            }
+            ViewBag.isRoleSelected = true;
+            ViewBag.role = role;
+            ViewBag.rolename = allroles.SingleOrDefault(x => x.Id == role).Name;
+            
+            var selectedRole = roleManager.Roles.SingleOrDefault(x=>x.Id==role);
+
+            //var users = (IEnumerable<ApplicationUser>)userManager.Users.Select(x => x ).ToList();
+            //var users = userManager.Users.Select(x => x).ToList();
+            List<ApplicationUser> userfinal = new List<ApplicationUser>();
+            var usrf = userManager.Users.Select(x => x).ToList();
+            var usr2 = context.Roles.SingleOrDefault(x => x.Id == role);
+            var usr3 = usr2.Users.Select(x => x).ToList();
+
+           
+            
+            foreach(var item in usrf)
+            {
+                var flag = false;
+                usr3.ForEach(x => {
+                    if(x.UserId==item.Id && flag==false)
+                    {
+                        userfinal.Add(item);
+                        flag = true;
+                    }
+                });
+            }
+            List<ApplicationUser> userfinalL = new List<ApplicationUser>();
+            usrf.ForEach(x => userfinalL.Add(x));
+            userfinal.ForEach(x => userfinalL.Remove(x));
+            ViewBag.Modal = (IEnumerable<ApplicationUser>)userfinalL;
+            FormCollection coll = new FormCollection();
+            coll["role"] = role;
+            return View(coll);
+        }
+        [HttpPost]
+        public ActionResult AssignRole(FormCollection collection)
+        {
+            //var user = userManager.FindById(collection["userid"]);
+            userManager.AddToRole(collection["userid"], collection["role"]);
+            return RedirectToAction("ViewUserRoles");
+        }
+        [HttpGet]
+        public ActionResult ViewUserRoles()
+        {
+            var allusers = userManager.Users.Select(x => x);
+            var allroles = roleManager.Roles.Select(x => x);
+            ViewBag.roles = allroles.ToList();
+            ViewBag.users = allusers.ToList();
+            return View();
         }
     }
 }
