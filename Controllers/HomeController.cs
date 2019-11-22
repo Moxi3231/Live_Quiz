20d9447a61209a57676e3f2987243cd30dd57ab8
Jaimin020 <48099245+Jaimin020@users.noreply.github.com>
2019-11-22 19:58:18 +0530

    Bug fix

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,12 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Web;
+using System.Web.Mvc;
+using Live_Quiz.Models;
+using System.Collections;
+
 
 namespace Live_Quiz.Controllers
 {
@@ -6,6 +14,22 @@ namespace Live_Quiz.Controllers
     {
         public ActionResult Index()
         {
+            DataModel db = new DataModel();
+            List<Quiz> lq;
+            lq = db.Quizs.Where(q => q.isPublic == true).ToList();
+            foreach (var q in lq)
+            {
+                if (!PinData.ht.ContainsValue(q.Id))
+                {
+                    PinData.ht.Add(PinData.pin, q.Id);
+                    PinData.qql.Add(PinData.pin, new List<Question>());
+                    QuizPlayers.lu.Add(PinData.pin, new ArrayList());
+                    UserAns.ans.Add(PinData.pin, new Hashtable());
+                    Live.qon.Add(PinData.pin, "f");
+                    Live.qs.Add(PinData.pin, "t");
+                    UserAns.score.Add(PinData.pin++, new Hashtable());
+                }
+            }
             return View();
         }
 
