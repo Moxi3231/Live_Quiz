6578fe153f1e7efe6a15d1f8f4881d2336ed32b0
Aatish chaudhari <40951689+Aatish13@users.noreply.github.com>
2019-11-25 23:39:10 +0530

    controller added

---
+++ Controllers/OnePlayerModeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Live_Quiz.Controllers
+{
+    public class OnePlayerModeController : Controller
+    {
+        // GET: OnePlayerMode
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
